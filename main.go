e4f61900f15d10c50c8f7f27aa014523d4959344
zhangdapeng520 <pygosuperman@outlook.com>
2022-08-22 14:24:56 +0800

    优化：代码优化

--- main.go
+++ main.go
@@ -3,16 +3,17 @@ package main
 import (
 	"github.com/zhangdapeng520/zdpgo_cmd"
 	"github.com/zhangdapeng520/zdpgo_tar"
+	"log"
 	"path/filepath"
 )
 
 var (
 	rootCmd   = &zdpgo_cmd.Command{}
-	tar       bool
-	untar     bool
-	directory string
-	file      string
-	Tar       *zdpgo_tar.Tar
+	tar       bool           // 是否为压缩
+	untar     bool           // 是否为解压缩
+	directory string         // 目录
+	file      string         // 文件
+	Tar       *zdpgo_tar.Tar // 压缩对象
 )
 
 func init() {
@@ -22,6 +23,7 @@ func init() {
 	rootCmd.Flags().StringVarP(&file, "file", "f", "", `the directory name for compress`)
 }
 
+// 主方法
 func main() {
 	// 创建一个根cmd对象
 	rootCmd.Run = func(cmd *zdpgo_cmd.Command, args []string) {
@@ -40,29 +42,31 @@ func main() {
 	rootCmd.Execute()
 }
 
+// 处理解压缩
 func handleUnTar() {
 	if file == "" {
 		err := Tar.UnTarGzDir(directory)
 		if err != nil {
-			c.Log.Error("un tar current dir error", "error", err, "directory", directory)
+			log.Println("un tar current dir error", "error", err, "directory", directory)
 			return
 		}
 	} else {
-		c.Log.Debug("un compress", "file", file)
+		log.Println("un compress", "file", file)
 		err := Tar.UnTarGzToSameDir(file)
 		if err != nil {
-			c.Log.Error("un tar file error", "error", err, "file", file)
+			log.Println("un tar file error", "error", err, "file", file)
 			return
 		}
 	}
 }
 
+// 处理压缩
 func handleTar() {
 	// compress current directory all child directory
 	if directory == "./" {
 		err := Tar.TarGzDirAllFiles(directory)
 		if err != nil {
-			c.Log.Error("compress child directory error", "error", err)
+			log.Println("compress child directory error", "error", err)
 		}
 		return
 	}
@@ -71,8 +75,8 @@ func handleTar() {
 	_, fileName := filepath.Split(directory)
 	err := Tar.TarGz(directory, fileName+".tar.gz")
 	if err != nil {
-		c.Log.Error("compress directory to *.tar.gz error", "error", err, "directory", directory, "fileName", fileName)
+		log.Println("compress directory to *.tar.gz error", "error", err, "directory", directory, "fileName", fileName)
 		return
 	}
-	c.Log.Error("compress directory to *.tar.gz success", "directory", directory, "fileName", fileName)
+	log.Println("compress directory to *.tar.gz success", "directory", directory, "fileName", fileName)
 }
